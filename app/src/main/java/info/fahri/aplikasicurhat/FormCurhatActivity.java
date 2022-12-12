ec07368c757686766ca88b26387b53a875c501a5
Fahri Firdausillah <elfaatta@gmail.com>
2022-12-12 15:12:05 +0700

    tambah input konfirmasi pass

--- app/src/main/java/info/fahri/aplikasicurhat/FormCurhatActivity.java
+++ app/src/main/java/info/fahri/aplikasicurhat/FormCurhatActivity.java
@@ -4,13 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class FormCurhatActivity extends AppCompatActivity {
+
+    EditText edtCurhat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form_curhat);
+        edtCurhat = findViewById(R.id.edtCurhat);
     }
 
     public void postCurhat(View v){
--- app/src/main/java/info/fahri/aplikasicurhat/RegisterActivity.java
+++ app/src/main/java/info/fahri/aplikasicurhat/RegisterActivity.java
@@ -6,21 +6,37 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class RegisterActivity extends AppCompatActivity {
 
-    EditText edtRegEmail, edtRegPassword, edtRegNama;
+    EditText edtRegEmail, edtRegPassword, edtRegNama, edtRegConfirm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        edtRegEmail = findViewById(R.id.edtRegEmail);
+        edtRegNama = findViewById(R.id.edtRegNama);
+        edtRegPassword = findViewById(R.id.edtRegPassword);
+        edtRegConfirm = findViewById(R.id.edtRegConfirm);
     }
 
     public void register(View v){
-        startActivity(new Intent(this, DashboardActivity.class)
-                        .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-                        .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK));
+        String email, nama, password, confirm;
+        email = edtRegEmail.getText().toString();
+        nama = edtRegNama.getText().toString();
+        password = edtRegPassword.getText().toString();
+        confirm = edtRegConfirm.getText().toString();
+
+        if(password.equals(confirm)){
+            startActivity(new Intent(this, DashboardActivity.class)
+                    .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                    .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK));
+        } else {
+            Toast.makeText(this,"Password dan Konfirmasi tidak sama", Toast.LENGTH_LONG).show();
+        }
     }
 
     public void toLogin(View v){
